0514b65f6a6fb83a18f7228406078d7cc7264766
theUmeshC <umesh.c@celestialsys.com>
2022-10-12 16:30:19 +0530

    cleaning up code at app.js

--- Frontend/shopify/src/App.jsx
+++ Frontend/shopify/src/App.jsx
@@ -101,7 +101,7 @@ function App() {
           <Route exact path="/">
             <ToastContainer
               position="top-right"
-              autoClose={5000}
+              autoClose={500}
               hideProgressBar={false}
               newestOnTop={false}
               closeOnClick
