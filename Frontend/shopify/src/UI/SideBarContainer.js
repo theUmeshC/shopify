8652478d0ca95338f6342efb045d1ce840319f21
theUmeshC <umesh.c@celestialsys.com>
2022-11-01 12:53:05 +0530

    sidebar ui updated

--- Frontend/shopify/src/UI/SideBarContainer.js
+++ Frontend/shopify/src/UI/SideBarContainer.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 
 export const SideBarContainer = styled.div`
   height: 90vh;
-  min-width: 160px;
+  width: 18%;
   position: sticky;
   top: 10vh;
-  background: #cacaca;
-  box-shadow: 50px 50px 100px #ebe9e9, -50px -50px 100px #ffffff;
+  background: #c2e2fe;
   flex-direction: column;
-  padding-left: 15px;
+  padding-left: 25px;
+  padding-top: 5px;
   box-sizing: border-box;
   overflow-y: scroll;
   ::-webkit-scrollbar {
