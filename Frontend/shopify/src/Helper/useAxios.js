9cbbc66d4ad69e5071607019b854a3a3a8ca3530
theUmeshC <umesh.c@celestialsys.com>
2022-11-04 15:25:35 +0530

    removed context

--- Frontend/shopify/src/Helper/useAxios.js
+++ Frontend/shopify/src/Helper/useAxios.js
@@ -2,26 +2,20 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loadData } from '../Store/productSlice';
-import { DataState } from '../Context/DataContext/dataContext';
 
 const useAxios = (baseURL) => {
   const dispatch = useDispatch();
-  const { productDataKey, filteredDataKey } = DataState();
-  const [, setProductData] = productDataKey;
-  const [, setFilteredData] = filteredDataKey;
   const [data, setData] = useState();
   const [loadingState, setLoadingState] = useState(true);
   useEffect(() => {
     axios.get(`${baseURL}`).then((response) => {
       setTimeout(() => {
         setData(response.data);
-        setProductData(response.data);
-        setFilteredData(response.data);
         dispatch(loadData(response.data));
         setLoadingState(false);
       }, 2500);
     });
-  }, [baseURL, setFilteredData, setProductData]);
+  }, [baseURL]);
   return { data, loadingState };
 };
 
--- Frontend/shopify/src/index.js
+++ Frontend/shopify/src/index.js
@@ -4,19 +4,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import Context from './Context/CartContext/context';
-import DataContext from './Context/DataContext/dataContext';
 import store from './Store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DataContext>
-      <Context>
         <Provider store={store}>
           <App />
         </Provider>
-      </Context>
-    </DataContext>
   </React.StrictMode>,
 );
