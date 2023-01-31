e8e4bf5fedf9bbef465aae901e31cb3f4cc4e37e
theUmeshC <umesh.c@celestialsys.com>
2023-01-31 13:20:58 +0530

    adding hooks explanation

--- Frontend/shopify/src/UI/HomeStyles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@mui/styles';
-
-export default makeStyles(() => ({
-  HomeWrapper: {
-    display: 'flex',
-    width: '100%',
-  },
-}));
--- Frontend/shopify/src/UI/Loading.js
+++
@@ -1,7 +0,0 @@
-import { makeStyles } from '@mui/styles';
-
-export default makeStyles(() => ({
-  loadingSidebar: {
-    width: '16%',
-  }
-}));
